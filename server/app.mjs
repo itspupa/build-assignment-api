d64b0b482f43385eeb7b6d001ca0f3cb836f4ad2
Anuna <anuaq20@gmail.com>
2025-09-02 13:27:26 +0700

    feat: Implement CRUD operations for assignments with error handling

--- server/app.mjs
+++ server/app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
@@ -10,7 +11,6 @@ app.get("/test", (req, res) => {
 });
 
 app.post("/assignments", async (req, res) => {
-  
 
   const newAssignment = {
     ...req.body,
@@ -24,7 +24,7 @@ app.post("/assignments", async (req, res) => {
       `insert into assignments (user_id, title, content, category, length, created_at, updated_at, published_at, status)
       values ($1, $2, $3, $4, $5, $6, $7, $8)`,
       [
-        1,
+        1, 
         newAssignment.title,
         newAssignment.content,
         newAssignment.category,
@@ -36,16 +36,99 @@ app.post("/assignments", async (req, res) => {
       ]
     );
   } catch {
-    return res
-      .status(500)
-      .json({
-        message:
-          "Server could not create assignment because database connection",
-      });
+    return res.status(500).json({
+      message: "Server could not create assignment because database connection",
+    });
   }
 
-  
   return res.status(201).json({ message: "Created assignment sucessfully" });
+});
+
+app.get("/assignments", async (req, res) => {
+ 
+  let results;
+
+  try {
+    results = await connectionPool.query(`select * from assignments`);
+  } catch {
+    return res.status(500).json({
+      message: "Server could not read assignment because database issue",
+    });
+  }
+
+  return res.status(200).json({
+    data: results.rows,
+  });
+});
+
+app.get("/assignments/:assignmentId", async (req, res) => {
+  
+  const assignmentIdFromClient = req.params.assignmentId;
+
+  const results = await connectionPool.query(
+    `
+		select * from assignments where assignment_id=$1
+	`,
+    [assignmentIdFromClient]
+  );
+
+  if (!results.rows[0]) {
+    return res.status(404).json({
+      message: `Server could not find a requested assignment (assignment id: ${assignmentIdFromClient})`,
+    });
+  }
+
+  return res.status(200).json({
+    data: results.rows[0],
+  });
+});
+
+app.put("/assignments/:assignmentId", async (req, res) => {
+
+
+  const assignmentIdFromClient = req.params.assignmentId;
+  const updatedAssignment = { ...req.body, updated_at: new Date() };
+ 
+  await connectionPool.query(
+    `
+      update assignments
+      set title = $2,
+          content = $3,
+          category = $4,
+          length = $5,
+          status = $6,
+          updated_at = $7
+      where assignment_id = $1
+    `,
+    [
+      assignmentIdFromClient,
+      updatedAssignment.title,
+      updatedAssignment.content,
+      updatedAssignment.category,
+      updatedAssignment.length,
+      updatedAssignment.status,
+      updatedAssignment.updated_at,
+    ]
+  );
+
+  return res.status(200).json({
+    message: "Updated assignment sucessfully",
+  });
+});
+
+app.delete("/assignments/:assignmentId", async (req, res) => {
+
+  const assignmentIdFromClient = req.params.assignmentId;
+
+  await connectionPool.query(
+    `delete from assignments
+	   where assignmentId = $1`,
+    [assignmentIdFromClient]
+  );
+
+  return res.status(200).json({
+    message: "Delete assignment sucessfully",
+  });
 });
 
 app.listen(port, () => {
